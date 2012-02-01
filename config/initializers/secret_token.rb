d8de44e58a3102a78c9e3977cf80df645d6263b2
Jesse Bounds <jesse@rebounds.net>
2012-02-01 22:29:43 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Govonym::Application.config.secret_token = '036c959af274c8f569961fa281a8b7febf00da3bfbd51df88bcf72e598e671bf37d4b9278a3f8de4f9c4fd158e7a6d799d8988f30c318d183878a94d9891fb4a'
